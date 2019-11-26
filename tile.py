d20a6bc1f68f1d99e8fde5a94edd9ea8e5b83799
sanforjr2021 <sanforjr2021@users.noreply.github.com>
2019-11-26 12:45:36 -0500

    Added TileLoot and removed TileFloor

--- tile.py
+++ tile.py
@@ -1,12 +1,11 @@
-class TileFloor:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.canMoveTo = True  # this is to allow it to check if it can move to
-
-
 class TileWall:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.canMoveTo = False  # this is to allow it to check if it can move to
+
+
+class TileLoot:
+    def __init__(self, x, y, points):
+        self.x = x
+        self.y = y
+        self.points = points
